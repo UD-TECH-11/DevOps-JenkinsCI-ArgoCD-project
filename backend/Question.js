d9786b21620834f55efc21140abd3de65363aaf9
UD-TECH-11 <damahee456@gmail.com>
2024-09-15 20:43:11 +0530

    Update Question.js

---
+++ backend/Question.js
@@ -0,0 +1,16 @@
+
+// Question.js
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  question: String,
+  option1: String,
+  option2: String,
+  option3: String,
+  option4: String,
+  ans: Number,
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+module.exports = Question;
